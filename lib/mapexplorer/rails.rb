a4a700073e2c53c32c0f32bd74e64cb2f33fde17
Adrien Montfort <adrien@stratumn.com>
2018-04-17 14:45:09 +0200

    update to mapexplorer-core 0.3.0
    - switch to npm/yarn for js libraries
    - include new evidence types
    - bump dependencies

--- lib/mapexplorer/rails.rb
+++ lib/mapexplorer/rails.rb
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "mapexplorer/rails/engine"
+require 'mapexplorer/rails/engine'
 
 module Mapexplorer
   module Rails
-    # Your code goes here...
   end
 end
--- vendor/assets/stratumn-agent-client/rollup.bower.min.config.js
+++
@@ -1,7 +0,0 @@
-import config from './rollup.bower.config';
-import uglify from 'rollup-plugin-uglify';
-
-config.plugins.push(uglify());
-config.dest = 'dist/stratumn-agent-client.min.js';
-
-export default config;
